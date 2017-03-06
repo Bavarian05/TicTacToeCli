db769a14948af98d808315f3980ddaa7777135e4
Rishi Shah <shah.rishi05@gmail.com>
2017-03-06 15:32:23 -0500

    Create game function & add psuedocode logic

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const prompt = require('prompt');
 
-
+// Declares and stores state of the board
 let board = {
   1: ' ',
   2: ' ',
@@ -14,7 +14,7 @@ let board = {
 };
 
 
-
+// Function that prints current state of the board
 const printBoard = () => {
   console.log('\n' +
   `${board[1]} | ${board[2]} | ${board[3]}` + '\n' +
@@ -25,3 +25,18 @@ const printBoard = () => {
 }
 
 printBoard();
+
+
+// Need a function to mark the board
+
+// Need a function to check if the board as winning combination
+
+// Need a function that run through a loop until
+
+// Function that sets up the game to play
+const play = () => {
+  prompt.start();
+  prompt.get([], function(err, result) {
+    //TODO game logic
+  })
+}
